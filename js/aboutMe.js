c8c41288a1039a52a062dd5717d28524268bb988
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-06-03 21:21:45 +0900

    Add validation for email address formats

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,29 +1,32 @@
 
-/*
-function darkMode(){ 
-    if(this.value === 'night'){
-        document.querySelector('#skills').style.backgroundColor = 'rgb(73, 73, 73)';
-        document.querySelector('#contact').style.backgroundColor = 'rgb(73, 73, 73)'; 
-        document.querySelector('body').style.backgroundColor = 'var(--text-color)';
-        document.querySelector('body').style.color = 'var(--background-color)';                    
-       this.value = 'day'
-    } else {
-        document.querySelector('#skills').style.backgroundColor = 'var(--color-gray)';
-        document.querySelector('#contact').style.backgroundColor = 'var(--color-gray)'; 
-        document.querySelector('body').style.backgroundColor = 'var(--background-color)';
-        document.querySelector('body').style.color = 'var(--text-color)';
-        this.value = 'night'
-        }
-}*/
-
-// Dark Mode toggle
+// Dark mode toggle
 function darkMode() {
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle("dark-mode");
  }
 
- // Confirm that mail has been sent
- function confirmSubmit(){
-     alert('메일이 전송되었습니다.')
- }
+ // Send the email and make alert pop-up 
+function sendingEmail(){
+    let check = document.getElementById('m_add');
+    let form = document.querySelector('.gform');
+
+    // Perform validation of email-address format
+    let exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+    if (exptext.test(check.value) == false) {
+        //이메일 형식이 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식이 아닐경우			
+        alert("입력한 메일형식이 올바르지 않습니다.");
+        check.focus();
+        return false;
+    }
+    
+    form.action = 'https://script.google.com/macros/s/AKfycbxzWE3vc5ziBuT4lJVYwQdcBlJxo8iImjAV8RWn/exec';
+    form.mothod = 'POST';
+
+    setTimeout(function (){
+        alert('메일이 전송되었습니다.');
+    }, 1000);
+}
+
+
+
  
